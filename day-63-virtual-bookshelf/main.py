9573e097276f3cf574c4455c9cc615bdebcd7315
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2022-02-04 11:57:33 +0000

    Add files via upload

---
+++ day-63-virtual-bookshelf/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    global all_books
+    return render_template('index.html', books=all_books)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    global all_books
+    # Add book details as a dictionary object to the all_books list
+    if request.method == 'POST':
+        new_book = {
+            "title": request.form['title'],
+            "author": request.form['author'],
+            "rating": request.form['rating']
+        }
+        all_books.append(new_book)
+
+        # You can use the redirect method from flask to redirect to another route
+        # e.g. in this case to the home page after the form has been submitted.
+        return redirect(url_for('home'))
+
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
