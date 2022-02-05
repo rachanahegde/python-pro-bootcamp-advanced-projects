519ada7756f1ffebf34e638e8a152d8b57e19a42
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2022-02-05 17:56:20 +0000

    Add files via upload

--- day-63-virtual-bookshelf/main.py
+++ day-63-virtual-bookshelf/main.py
@@ -1,31 +1,57 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# Create an SQLite database called library.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+# Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-all_books = []
+# Create a table called library with 4 fields: id, title, author and rating
+class Library(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', books=all_books)
+    # Read all records
+    return render_template('index.html', books=Library.query.all())
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    # Add book details as a dictionary object to the all_books list
+    # Create a new record in the library table
     if request.method == 'POST':
-        new_book = {
-            "title": request.form['title'],
-            "author": request.form['author'],
-            "rating": request.form['rating']
-        }
-        all_books.append(new_book)
-
+        new_book = Library(title=request.form['title'], author=request.form['author'], rating=request.form['rating'])
+        db.session.add(new_book)
+        db.session.commit()
         # You can use the redirect method from flask to redirect to another route
         # e.g. in this case to the home page after the form has been submitted.
         return redirect(url_for('home'))
-
     return render_template('add.html')
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit_rating():
+    book_id = request.args.get('id')
+    if request.method == 'POST':
+        print(request.args.get('id'))
+        # print(Library.query.get())
+        # request.form['new_rating']
+        # db.session.commit()
+        # return redirect(url_for('home'))
+    return render_template('edit-rating.html', book_id=book_id, library=Library)
 
 
 if __name__ == "__main__":
