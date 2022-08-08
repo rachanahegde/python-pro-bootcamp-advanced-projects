7a54cae5d4b325b58a6ed3f6b6e0ed9f4c29dba7
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2022-08-08 20:45:46 +0800

    Add files via upload

--- day-66-building-rest-api/main.py
+++ day-66-building-rest-api/main.py
@@ -24,6 +24,20 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+        # Method 2. Alternatively use Dictionary Comprehension to do the same thing.
+        # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 @app.route("/")
 def home():
@@ -60,6 +74,9 @@ def get_random_cafe():
 
     # Other method of serialising  the database row Object to JSON is first converting it to a dictionary
     # then using jsonify() to convert the dictionary (which is  similar in structure to JSON) to a JSON.
+    # Convert the random_cafe data record to a dictionary of key-value pairs.
+    # return jsonify(cafe=random_cafe.to_dict())
+
 
 # HTTP GET - All the Cafes
 @app.route("/all")
@@ -68,14 +85,68 @@ def get_all_cafes():
     # Use list comprehension
     return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
 
+
 # HTTP GET - Find a Cafe
+@app.route("/search")
+def search_cafe():
+    loc = request.args.get("loc")
+    cafes = Cafe.query.all()
+    for cafe in cafes:
+        if cafe.location == loc:
+            return jsonify(cafe=cafe.to_dict())
+
+    return jsonify(error={
+        "Not Found": "Sorry, we don't have a cafe at that location."
+    })
 
 
 # HTTP POST - Create Record
+@app.route("/add", methods=["POST"])
+def add_cafe():
+    new_cafe = Cafe(name=request.form.get("name"),
+                    location=request.form.get("loc"),
+                    map_url=request.form.get("map_url"),
+                    img_url=request.form.get("img_url"),
+                    seats=request.form.get("seats"),
+                    has_toilet=bool(request.form.get("has_toilet")),
+                    has_wifi=bool(request.form.get("has_wifi")),
+                    has_sockets=bool(request.form.get("has_sockets")),
+                    can_take_calls=bool(request.form.get("can_take_calls")),
+                    coffee_price=request.form.get("coffee_price"))
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
 
-# HTTP PUT/PATCH - Update Record
 
-# HTTP DELETE - Delete Record
+# HTTP PATCH - Update Coffee Price for a Cafe Record
+@app.route("/update-price/<cafe_id>", methods=["GET", "PATCH"])
+def update_price(cafe_id):
+    cafe = db.session.query(Cafe).get(cafe_id)
+    new_price = request.args.get("new_price")
+    try:
+        cafe.coffee_price = new_price
+    except AttributeError:
+        # Pass an HTTP code with your response: #404 = Resource not found
+        return jsonify(error={"Not Found": "Sorry, a cafe with that id was not found in the database."}), 404
+    else:
+        db.session.commit()
+        # 200 = Ok
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+
+
+# HTTP DELETE - Delete Record for a Closed Cafe
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    if request.args.get("api_key") == "TopSecretAPIKey":
+        cafe_to_delete = Cafe.query.get(cafe_id)
+        if cafe_to_delete:
+            db.session.delete(cafe_to_delete)
+            db.session.commit()
+            return jsonify(response={"Success": "The cafe was deleted from the database."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+    else:
+        return jsonify(response={"error": "Sorry, that's not allowed. Make sure you have the correct API key."}), 403
 
 
 if __name__ == '__main__':
